efc3acf728c8c3469960f8e8a17118fbf81a043c
Himanshu Shukla <hshukla2611@gmail.com>
2023-02-05 21:00:45 +0530

    adding conditions for log controller

--- server/src/controllers/logsController.js
+++ server/src/controllers/logsController.js
@@ -5,46 +5,101 @@ const { rmvDoubleQuotes } = require('../utils/quickutils');
 
 const getLogs = catchAsyncErrors(async (req, res, next) => {
     const logs = await Logs.find();
-    res.status(200).json({
-        success: true,
-        message: "get log reached",
-        Logs: logs
-    });
+    if (logs != null) {
+        res.status(200).json({
+            success: true,
+            message: "get log reached",
+            Logs: logs
+        });
+    } else {
+        res.status(400).json({
+            success: false,
+            message: "no logs found"
+        });
+    }
 });
 
 const getLogsForCurrentUser = catchAsyncErrors(async (req, res, next) => {
     const userId = verifyToken(req.cookies.token);
-    const logs = await Logs.find({ user: rmvDoubleQuotes(userId) });
-    res.status(200).json({
-        success: true,
-        message: "get log for user reached",
-        userLogs: logs
-    });
+    if (userId != null) {
+        const logs = await Logs.find({ user: rmvDoubleQuotes(userId) });
+        res.status(200).json({
+            success: true,
+            message: "get log for user reached",
+            userLogs: logs
+        });
+    } else {
+        res.status(400).json({
+            success: false,
+            message: "User not logged in",
+        });
+    }
 });
 
 const createLog = catchAsyncErrors(async (req, res, next) => {
-    const userId = verifyToken(req.cookies.token);
-    const completeLog = { ...req.body, user: rmvDoubleQuotes(userId) };
-    const log = await Logs.create(completeLog);
-    res.status(200).json({
-        success: true,
-        message: "create log reached",
-        log: log
-    });
+    const token = req.cookies.token;
+    if (token != null) {
+        const userId = verifyToken(req.cookies.token);
+        const completeLog = { ...req.body, user: rmvDoubleQuotes(userId) };
+        const log = await Logs.create(completeLog);
+        res.status(200).json({
+            success: true,
+            message: "create log reached",
+            log: log
+        });
+    } else {
+        res.status(400).json({
+            success: false,
+            message: "User not logged in",
+        });
+    }
 });
 
 const updateLog = catchAsyncErrors(async (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        message: "update log reached",
-    });
+    // TODO: check data getting from frontend
+    const newData = req.body;
+    if (newData !== null) {
+        const id = req.params.id;
+        const userId = verifyToken(req.cookies.token);
+        const currentLog = await Logs.findOneAndUpdate(
+            { _id: id, user: rmvDoubleQuotes(userId) },
+            { newData }
+        );
+        if (currentLog != null) {
+            res.status(200).json({
+                success: true,
+                message: "update log reached",
+                log: currentLog
+            });
+        } else {
+            res.status(400).json({
+                success: false,
+                message: "Some internal error occured"
+            });
+        }
+    } else {
+        res.status(400).json({
+            success: false,
+            message: "No data provided in the body"
+        })
+    }
 });
 
 const deleteLog = catchAsyncErrors(async (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        message: "delete log reached",
-    });
+    const id = req.params.id;
+    const userId = verifyToken(req.cookies.token);
+    const currentLog = await Logs.findOneAndDelete({ _id: id, user: rmvDoubleQuotes(userId) });
+    if (currentLog) {
+        res.status(200).json({
+            success: true,
+            message: "log deleted successfully",
+        });
+    } else {
+        res.status(400).json({
+            success: false,
+            message: "Some internal error occured"
+        });
+    }
 });
 
 module.exports = {
